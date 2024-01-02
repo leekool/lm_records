976182a12c5fcd8c5609a10d1e25578a36b3445f
leekool <lee@imre.al>
2024-01-03 00:44:20 +1100

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import "../app.css";
+</script>
+
+<div class="main">
+    <div class="logo">
+        <img src="/images/logo.png" alt="logo" />
+    </div>
+</div>
+
+<style>
+    .main {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .logo {
+        display: flex;
+        align-self: center;
+    }
+</style>
